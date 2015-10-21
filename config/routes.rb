8be90c4cdfc9523f1e04c82f9b2035380e7bf5e7
cycorld <charleschoi87@gmail.com>
2015-10-21 11:44:59 +0900

    home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
   resources :nuerons
   # The priority is based upon order of creation: first created -> highest priority.
