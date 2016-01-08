53672df3005e03879c9f080f37574a99f24efcd3
cycorld <charleschoi87@gmail.com>
2016-01-09 04:16:45 +0900

    Nerones self relations

--- app/models/nueron.rb
+++ app/models/nueron.rb
@@ -2,4 +2,9 @@ class Nueron < ActiveRecord::Base
   has_many :active_links, class_name: "Synapse",
                           foreign_key: "origin_id",
                           dependent: :destroy
+  has_many :passive_links, class_name: "Synapse",
+                           foreign_key: "next_id",
+                           dependent: :destroy
+  has_many :next, through: :active_links, source: :next
+  has_many :prev, through: :passive_links, source: :origin
 end
--- app/models/synapse.rb
+++ app/models/synapse.rb
@@ -1,6 +1,6 @@
 class Synapse < ActiveRecord::Base
-  belongs_to :origin, class_name: "User"
-  belongs_to :next, class_name: "User"
+  belongs_to :origin, class_name: "Nueron"
+  belongs_to :next, class_name: "Nueron"
   validates :origin_id, presence: true
   validates :next_id, presence: true
 end
