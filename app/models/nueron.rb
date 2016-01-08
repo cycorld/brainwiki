8e8c2465fd101a382a7ce54b15572bcc55ae18ca
cycorld <charleschoi87@gmail.com>
2016-01-09 03:35:37 +0900

    nueron and synapse relations

--- app/models/nueron.rb
+++ app/models/nueron.rb
@@ -1,2 +1,5 @@
 class Nueron < ActiveRecord::Base
+  has_many :active_links, class_name: "Synapse",
+                          foreign_key: "prev_id",
+                          dependent: :destroy
 end
--- app/models/synapse.rb
+++ app/models/synapse.rb
@@ -1,2 +1,4 @@
 class Synapse < ActiveRecord::Base
+  belongs_to :prev, class_name: "User"
+  belongs_to :next, class_name: "User"
 end
