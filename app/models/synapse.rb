ba30b72981ad0041cd5eb334b6295f09111b3d17
cycorld <charleschoi87@gmail.com>
2016-01-09 04:03:11 +0900

    synapase ids validates test OK

--- app/models/synapse.rb
+++ app/models/synapse.rb
@@ -1,4 +1,6 @@
 class Synapse < ActiveRecord::Base
   belongs_to :origin, class_name: "User"
   belongs_to :next, class_name: "User"
+  validates :origin_id, presence: true
+  validates :next_id, presence: true
 end
