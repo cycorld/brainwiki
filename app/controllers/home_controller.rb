f5a7d00167456e77d8110684447f7fc7d1a298f5
cycorld <charleschoi87@gmail.com>
2016-01-09 23:38:50 +0900

    Nueron show by token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
     @nuerons = Nueron.all
     @synapses = Synapse.all
   end
+
+  def show
+    @nueron = Nueron.find_by_token(params[:token])
+    render 'nuerons/show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :nuerons
   get 'nuerons/:origin_id/to/:next_id' => 'nuerons#link'
+  get 'show/:token' => 'home#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
