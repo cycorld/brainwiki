5f3c9882927ccfddc7c132850da808c8fe733548
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-10 15:35:56 +0900

    quiz의 show페이지를 만들어서 만든 퀴즈를 볼 수 있음
     - 버튼들 meterialize시킴

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,6 @@
 class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:show]
+
   def new
     @nueron = Nueron.find(params[:nueron_id])
     @quiz  = Quiz.new
@@ -6,26 +8,28 @@ class QuizzesController < ApplicationController
 
   def create
     @nueron = Nueron.find(params[:nueron_id])
-   # @quiz = Quiz.new(quiz_params)
-    @quiz = Quiz.new
-    @quiz.user_id = current_user.id
-    @quiz.nueron_id = @nueron.id
-    @quiz.question = params[:quiz][:question]
-    @quiz.answers = JSON.parse(params[:quiz][:answers])
+    quiz = Quiz.new
+    quiz.user_id = current_user.id
+    quiz.nueron_id = @nueron.id
+    quiz.question = params[:quiz][:question]
+    quiz.answers = JSON.parse(params[:quiz][:answers])
     arr = params[:quiz][:answers]
     puts "*******************************"
     puts arr
-  #  @quiz.answers = params[:quiz][:answers]
-    @quiz.order = params[:quiz][:order]
-    @quiz.save
-    redirect_to @nueron
+    quiz.order = params[:quiz][:order]
+    quiz.save
+    redirect_to nueron_quiz_path(@nueron, quiz)
   end
 
   def show
-    @quiz = Quiz.find(params[:nueron_id][:id])
+    @nueron = Nueron.find(params[:nueron_id])
   end
 
   private
+    def set_quiz
+      @quiz = Quiz.find_by(id: params[:id], nueron_id: params[:nueron_id])
+    end
+
     def quiz_params
       params.require(:quiz).permit(:question, :answers, :order)
     end
