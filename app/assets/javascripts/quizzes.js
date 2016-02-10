55d476c0cf131c8bdf964f913b06b1e46b9951b3
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-11 00:28:23 +0900

    quiz의 answers를 hash로 키값을 array, correct로 나눔
     - array에는 답안들이 들어가고, correct에는 정답인지 아닌지가 들어감
     - serialize, store method 이용.

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -1,16 +1,5 @@
 var answerArray = new Array();
 
-$(document).on('ready page:load', function(){
-  $('.foranswer').pressEnter(function(){
-    $('#answerform').append(
-     $('<input>', { type:'text'})
-      );
-    var value = this.value;
-    answerArray.push(value);
-   $('.sth').html(answerArray);
-  });
-});
-
 // 옆에 add 버튼 누를시
 $(document).on('ready page:load', function(){
   $('.actions').click(function(){
@@ -29,15 +18,3 @@ $(document).on('ready page:load', function(){
   });
 });
 
-// press enter -> create event
-$.fn.pressEnter = function(fn) {
-  return this.each(function() {
-    $(this).bind('enterPress', fn);
-    $(this).keyup(function(e){
-      if(e.keyCode == 13)
-    {
-      $(this).trigger("enterPress");
-    }
-    })
-  });
-};
