b35988e5d1daf4742699b52be44ec14f63a19566
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-13 00:21:42 +0900

    퀴즈를 풀어서 success하면 초록색으로 질문 칸에 경계선이 생김

--- app/assets/javascripts/nueron.js
+++ app/assets/javascripts/nueron.js
@@ -4,15 +4,16 @@ $(document).ready(function(){
   });
 });
 
+
 $(document).ready(function(e) {
-
   $("form[ajax=true]").submit(function(e) {
-
+    var quiz_id = $('input:checked').siblings('.hidden_quiz_id').val();
     e.preventDefault();
-
     var form_data = $(this).serialize();
     var form_url = $(this).attr("action");
     var form_method = $(this).attr("method").toUpperCase();
+    console.log($('.collapsible-header.'+quiz_id));
+    $('.collapsible-header.'+quiz_id).css("border", "2px solid yellowgreen");
     $.ajax({
       url: form_url,
       type: form_method,
