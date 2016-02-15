49fa785cbf87700bae09ed1267164d1f87674a1c
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-15 17:46:31 +0900

    modify the quiz state with border
     - after solving quiz, the quiz card became inactive and user cannot click the quiz content, only with refreshing he or she can click and solve again

--- app/assets/javascripts/nueron.js
+++ app/assets/javascripts/nueron.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
 
 $(document).ready(function(e) {
   $("form[ajax=true]").submit(function(e) {
-    var quiz_id = $('input:checked').siblings('.hidden_quiz_id').val();
+//  var quiz_id = $('.collapsible-header.'+quiz_id+'.active').next('.collapsible-body').find('input:checked');
+//    var quiz_id = $('.collapsible-header.'+quiz_id+'.active').data("id");
+    var quiz_id = $('.collapsible-header.active').data("id");
     e.preventDefault();
     var form_data = $(this).serialize();
     var form_url = $(this).attr("action");
@@ -21,6 +23,9 @@ $(document).ready(function(e) {
         if(res.success) {
           Materialize.toast('정답', 5000)
           $('.collapsible-header.'+quiz_id).css("border", "2px solid yellowgreen");
+          $('.collapsible-header.'+quiz_id).prop('disabled',true);
+          $('.collapsible.popout li').removeClass("active");
+          $('.collapsible-body').css("display","none");
         } else {
           Materialize.toast('오답', 5000)
           $('.collapsible-header.'+quiz_id).css("border", "2px solid red");
