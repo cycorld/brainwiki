bb7f77abb2ccf27310532db46eab9bee20df5f35
ISEOB KIM <dltjq0730@unist.ac.kr>
2016-02-15 17:48:31 +0900

    add quiz_history model

---
+++ db/migrate/20160215084804_create_quiz_histories.rb
@@ -0,0 +1,14 @@
+class CreateQuizHistories < ActiveRecord::Migration
+  def change
+    create_table :quiz_histories do |t|
+      t.integer, :quiz_id
+      t.integer, :nueron_id
+      t.integer, :user_id
+      t.boolean, :correct
+      t.string, :question
+      t.string :answer
+
+      t.timestamps null: false
+    end
+  end
+end
